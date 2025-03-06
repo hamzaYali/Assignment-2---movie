46f2e2fe143bdd43d827560da09e2499869f514d
hamzaYali <98500914+hamzaYali@users.noreply.github.com>
2025-03-06 12:00:22 -0600

    Update settings.py

--- djangoProject1/settings.py
+++ djangoProject1/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5gdz!cz)+n-qwn+o*j^#(4iy%u4@$#@vln8zol$vc(6ifi&joq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
